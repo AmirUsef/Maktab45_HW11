1e910aa804669c2bf7a39359c357b12d836ebcb3
Amir Yousef Heydari <ayh1380@gmail.com>
2021-01-25 18:47:10 +0330

    question 4 results added

--- 4/app.js
+++ 4/app.js
@@ -19,3 +19,18 @@ numbers.forEach(element => {
     let number = element.split(" - ")[1];
     users.find(item => item.id === id).numbers.push(number);
 });
+let results = ""
+for (let index = 0; index < users.length; index++) {
+    const user = users[index];
+    results += user.name;
+    if (user.numbers.length == 0)
+        results += " hasn’t any phone number.\n";
+    else if (user.numbers.length == 1)
+        results += `’s phone number is ${user.numbers[0]}.\n`;
+    else {
+        results += "’s phone numbers are ";
+        user.numbers.forEach(element => results += `${element}, `);
+        results = results.slice(0, -2).concat(".\n");
+    }
+}
+fs.writeFileSync('files/results.txt', results);
